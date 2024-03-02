26e8afd920579f9f820db2470b2889d2f9809d21
sasikanth777 <sasikanth1531@gmail.com>
2024-03-02 15:25:40 +0530

    added

--- mon.py
+++ mon.py
@@ -35,5 +35,6 @@ def index():
     if cpu_metric > 80 or mem_metric > 80:
         Message = "High CPU or Memory Detected, scale up!!!"
     return render_template("index.html", cpu_metric=cpu_metric, mem_metric=mem_metric, message=Message)
+
 if __name__=='__main__':
     app.run(debug=True, host = '0.0.0.0')
